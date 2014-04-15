25765c8b97f090b7d05ff581de54054ef3d8fccc
李佳 <lijia@lijiadeMacBook-Pro.local>
2014-04-15 21:16:54 +0800

    Initial Commit

---
+++ declType/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  declType
+//
+//  Created by 李佳 on 14-4-15.
+//  Copyright (c) 2014年 LiJia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
